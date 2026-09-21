0c3cd6b79ed59beea706e855dc3ad16d988054b2
Mengli Xiao12 <xiaox345@umn.edu>
2026-09-21 14:38:48 -0500

    Generated a package manual

---
+++ R/repMeta-package.R
@@ -0,0 +1,18 @@
+#' repMeta: Quantifying Replicability of Multiple Studies in a Meta-Analysis
+#'
+#' @description
+#' \loadmathjax
+#'
+#' The package provides functionality for replicability analysis in a meta-analysis
+#' as described in Xiao et al. (2024)
+#'
+#' @docType package
+#' @name repMeta
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
