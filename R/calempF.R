58661c5918472e814e8b6e81e0a8a4324936e922
Mengli Xiao12 <xiaox345@umn.edu>
2026-09-21 11:33:32 -0500

    Modify to generalize the parallel implementation to windows.

--- R/calempF.R
+++ R/calempF.R
@@ -20,18 +20,36 @@ bootdat <- function(dat1,SEED,tau2){
 #' # 500 bootstrapped \eqn{R_1} values
 #' calemF(dat1,1,nb=500)
 #' @export
-calempF <- function(dat1,m,nb=1000){
+calempF <- function(dat1,m,nb=1000,n.cores=getOption("mc.cores",2L)){
+  n.cores <- min(n.cores,parallel::detectCores(logical = TRUE))
+
   set.seed(9876)
   tau2 <- meta.reml(dat1)$tau2
   ## 1000 bootstrap runs
   seed.ls <- sample(1:200000,nb)
-  Rmls <- parallel::mclapply(seed.ls,function(SEED){
-    dat1.boot <- bootdat(dat1,SEED,tau2)
-    ## leave one study out
-    maxR <- calR(dat1.boot,m)
-    Rm <- maxR$maxR
-    return(Rm)
-  },mc.cores=6)
+
+  chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_","")
+  if (nzchar(chk) && chr != "false")
+    n.cores <- min(n.cores,2L)
+  if (n.cores > 1L){
+    cl <- parallel::makeCluster(n.cores)
+    on.exit(parallel::stopCluster(cl),add=TRUE)
+    Rmls <- parallel::parLapply(cl,seed.ls,function(SEED,dat1,tau2,m){
+      dat1.boot <- repMeta::bootdat(dat1,SEED,tau2)
+      ## leave one study out
+      maxR <- repMeta::calR(dat1.boot,m)
+      Rm <- maxR$maxR
+      return(Rm)
+    },dat1=dat1,tau2=tau2,m=m)
+  }else{
+    Rmls <- lapply(seed.ls,function(SEED){
+      dat1.boot <- bootdat(dat1,SEED,tau2)
+      maxR <- calR(dat1.boot,m)
+      Rm <- maxR$maxR
+      return(Rm)
+    })
+  }
+
   ## use bootstrap to estimate the sample variance
   Rval2 <- data.frame(do.call(cbind,Rmls))
   Rval2 <- as.vector(Rval2)
