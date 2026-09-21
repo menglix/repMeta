f88d59cab4305259d0a5e2e75a0bb15ec9186b7f
Mengli Xiao12 <xiaox345@umn.edu>
2026-09-21 12:01:15 -0500

    fix typo for namespaces and if statement

--- R/calempF.R
+++ R/calempF.R
@@ -29,13 +29,13 @@ calempF <- function(dat1,m,nb=1000,n.cores=getOption("mc.cores",2L)){
   seed.ls <- sample(1:200000,nb)
 
   chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_","")
-  if (nzchar(chk) && chr != "false")
+  if (nzchar(chk) && chk != "false")
     n.cores <- min(n.cores,2L)
   if (n.cores > 1L){
     cl <- parallel::makeCluster(n.cores)
     on.exit(parallel::stopCluster(cl),add=TRUE)
     Rmls <- parallel::parLapply(cl,seed.ls,function(SEED,dat1,tau2,m){
-      dat1.boot <- bootdat(dat1,SEED,tau2)
+      dat1.boot <- repMeta:::bootdat(dat1,SEED,tau2)
       ## leave one study out
       maxR <- repMeta::calR(dat1.boot,m)
       Rm <- maxR$maxR
